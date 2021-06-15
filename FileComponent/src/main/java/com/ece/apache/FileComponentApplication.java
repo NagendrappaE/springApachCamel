fbd83ff3b663abbc12aecb7e3729f0fa5d0fe793
NagendrappaE-evolvus <nagendrappa.e@evolvussolutions.com>
2021-06-15 16:43:07 +0530

    file componenet

---
+++ FileComponent/src/main/java/com/ece/apache/FileComponentApplication.java
@@ -0,0 +1,13 @@
+package com.ece.apache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileComponentApplication.class, args);
+	}
+
+}
---
+++ FileComponent/src/test/java/com/ece/apache/FileComponentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ece.apache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileComponentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
