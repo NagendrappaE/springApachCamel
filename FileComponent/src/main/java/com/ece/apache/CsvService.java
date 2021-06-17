11da22beccacc599e8700152bb3457216a344859
NagendrappaE-evolvus <nagendrappa.e@evolvussolutions.com>
2021-06-17 10:44:39 +0530

    csv reader

---
+++ FileComponent/src/main/java/com/ece/apache/CsvService.java
@@ -0,0 +1,12 @@
+package com.ece.apache;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CsvService {
+	
+	public void input(Object ob) {
+		System.out.println("input");
+	}
+
+}
--- FileComponent/src/test/java/com/ece/apache/FileComponentApplicationTests.java
+++ FileComponent/src/test/java/com/ece/apache/FileComponentApplicationTests.java
@@ -1,12 +1,15 @@
 package com.ece.apache;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class FileComponentApplicationTests {
 
 	@Test
+	
 	void contextLoads() {
 	}
 
